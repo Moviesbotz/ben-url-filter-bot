ab757ab9761a7a5bc01e51832ec8dd1ad82f1846
Ashok <121341606+Moviesbotz@users.noreply.github.com>
2023-02-12 14:30:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Moviesbotz/Auto-filter-Urlshortener-All.git /Auto-filter-Urlshortener-All
+  git clone https://github.com/Moviesbotz/ben-url-filter-bot.git /ben-url-filter-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-filter-Urlshortener-All
+  git clone $UPSTREAM_REPO /ben-url-filter-bot
 fi
-cd /Auto-filter-Urlshortener-All
+cd /ben-url-filter-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
